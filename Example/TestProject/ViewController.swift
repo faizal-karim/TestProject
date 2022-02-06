3022610ce8c6c699b6e5359a45ba1c90bad7881a
Faizal <faizal.karim@bank-abc.com>
2022-02-06 16:57:06 +0530

    Initial commit

---
+++ Example/TestProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Faizal on 02/06/2022.
+//  Copyright (c) 2022 Faizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
